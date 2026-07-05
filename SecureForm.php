a0496453d207867be05cc10943e6686e6f0d6d70
Greg H <grarha@gmail.com>
2026-07-04 22:36:01 -0700

    Added function to display form, smoke tested

--- SecureForm.php
+++ SecureForm.php
@@ -30,10 +30,25 @@
                 $retVal = filter_var($data, FILTER_SANITIZE_EMAIL);
                 if (!filter_var($retVal, FILTER_VALIDATE_EMAIL)) {
                     echo "\"$data\" is not a valid email address. <br />\n";
-                    ++$errorCount;
+                    ++$errorCount;                    
                 }
             }
             return($retVal);
         }
 
-        
+        function displayForm($Name, $Email, $Topic, $Message) {
+            ?><h2 style='text-align:center'>Send Message</h2>
+            <form name='message' action='' method='post'>
+                <input type='hidden' name='_token' value='<?php echo csrf_token(); ?>'>
+                <p>Full name:
+                    <input type='text' name='Name' value='<?php echo $Name; ?>' /></p>
+                <p>Email address:
+                    <input type='text' name='Email' value='<?php echo $Email; ?>' /></p>
+                <p>Topic of message:
+                    <input type='text' name='Topic' value='<?php echo $Topic; ?>' /></p>
+                <p>Message:<br />
+                    <textarea name='Message'><?php echo $Message; ?></textarea></p>
+                <p><input type='reset' value='Clear Form' />&nbsp; &nbsp;
+                    <input type='submit' name='Submit' value='Send Form' /></p>
+            </form>
+        <?php }
